f3341c1862f4c0c7fd7241850bf31a61ec995553
areebqazi <66074608+areebqazi@users.noreply.github.com>
2023-01-16 00:54:23 +0530

    Time: 7 ms (9.68%), Space: 39.9 MB (61.39%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int reverse(int x) {
+        char sign='+';
+        if(x<0) sign='-';
+        StringBuilder sb=new StringBuilder(x+"");
+        String c;
+        if(sb.charAt(0)=='-'){
+            sb.deleteCharAt(0);
+            c="2147483648";
+        }
+        else
+            c="2147483647";
+        int j=sb.length()-1;
+       for(int i=0;i<sb.length()/2;i++){
+           char k=sb.charAt(j);
+           sb.setCharAt(j,sb.charAt(i));
+           sb.setCharAt(i,k);
+           j--;
+       }
+        System.out.println(sb);
+        if(sb.length()<10){
+            if(sign=='-')
+            sb.insert(0,'-');
+            return Integer.parseInt(sb.toString());
+        }
+        // to check outwards range
+        int i=0;
+        while(i<c.length()){
+            if(sb.charAt(i)>c.charAt(i))
+                return 0;
+            else if(sb.charAt(i)<c.charAt(i))
+                break;
+            i++;
+        }
+        if(sign=='-')
+            sb.insert(0,'-');
+        return Integer.parseInt(sb.toString());
+    }
+}
