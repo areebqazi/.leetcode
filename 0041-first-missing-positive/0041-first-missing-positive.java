40dc86f939ab9eb9d278fbc71e22ec3d4e3bf39c
areebqazi <66074608+areebqazi@users.noreply.github.com>
2023-02-04 01:31:56 +0530

    Time: 45 ms (5.12%), Space: 51.4 MB (37.79%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,11 +1,11 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        if(nums.length==1 && nums[0]==0)
-            return 1;
-        if(nums.length==1 && nums[0]==1)
-            return 2;
-         if(nums.length==1 && nums[0]!=0 && nums[0]!=1)
-             return 1;
+         // if(nums.length==1 && nums[0]==0)
+         //    return 1;
+         // if(nums.length==1 && nums[0]==1)
+         //    return 2;
+         // if(nums.length==1 && nums[0]!=0 && nums[0]!=1)
+         //     return 1;
         Set<Integer> set = new TreeSet<>();
         ArrayList<Integer>a=new ArrayList<>();
         for(int i=0;i<nums.length;i++){
@@ -16,7 +16,7 @@ class Solution {
             a.add(x);
         if(a.size()==0)
             return 1;
-        if(a.get(0)!=1)
+        if(a.get(0)!=1 || a.get(0)==0)
             return 1;
         for(int i=0;i<a.size()-1;i++){
             if(a.get(i+1)!=a.get(i)+1)
