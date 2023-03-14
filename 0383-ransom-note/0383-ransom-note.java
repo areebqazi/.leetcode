e4f7d664e6bc4e9a06401c63f480eedfa556cf47
areebqazi <66074608+areebqazi@users.noreply.github.com>
2023-03-14 16:25:56 +0530

    Time: 4 ms (65.89%), Space: 42.4 MB (86.60%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int a[]=new int[26];
+        int b[]=new int[26];
+        for(int i=0;i<ransomNote.length();i++){
+            a[ransomNote.charAt(i)-97]++;
+        }
+        
+        for(int i=0;i<magazine.length();i++){
+            b[magazine.charAt(i)-97]++;
+        }
+        for(int i=0;i<26;i++){
+            if(a[i]>b[i])
+                return false;
+        }
+        return true;
+    }
+}
