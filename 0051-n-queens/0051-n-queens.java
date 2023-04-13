4af5cc2f28ee8e9cd392c76d5dd1c6583b86141b
areebqazi <66074608+areebqazi@users.noreply.github.com>
2023-04-13 14:47:30 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (92.46%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,48 +1,50 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        char[][] board = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = '.';
+        char[][]board=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
             }
         }
-        queen(0, n, board, ans);
+
+        List<List<String>>ans=new ArrayList<>();
+        int leftrow[]=new int[n];
+        int upperDiagonal[]=new int[2*n-1];
+        int lowerDiagonal[]=new int[2*n-1];
+        solve(0,ans,board,leftrow,upperDiagonal,lowerDiagonal);
         return ans;
+
     }
-    
-    public void queen(int col, int n, char[][] board, List<List<String>> ans) {
-        if (col == n) {
-            List<String> solution = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                solution.add(new String(board[i]));
-            }
-            ans.add(solution);
+    public void solve(int col,List<List<String>>ans,char[][]board,int leftrow[],int upperDiagonal[],int lowerDiagonal[]){
+        if(col==board.length){
+            ans.add(construct(board));
             return;
         }
-        for (int row = 0; row < n; row++) {
-            if (isSafe(row, col, n, board)) {
-                board[row][col] = 'Q';
-                queen(col + 1, n, board, ans);
-                board[row][col] = '.';
+        for(int row=0;row<board.length;row++){
+            if(leftrow[row]==0 && upperDiagonal[board.length-1+col-row]==0 && lowerDiagonal[row+col]==0){
+                //put the queen
+                board[row][col]='Q';
+                leftrow[row]=1;
+                upperDiagonal[board.length-1+col-row]=1;
+                lowerDiagonal[row+col]=1;
+
+                //call the recursion 
+                solve(col+1,ans,board,leftrow,upperDiagonal,lowerDiagonal);
+
+                //backtracking part
+                board[row][col]='.';
+                leftrow[row]=0;
+                upperDiagonal[board.length-1+col-row]=0;
+                lowerDiagonal[row+col]=0;
             }
         }
     }
-    
-    public boolean isSafe(int row, int col, int n, char[][] board) {
-        int i, j;
-        for (i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q')
-                return false;
+    public List<String> construct(char [][] board){
+        List<String>cur=new ArrayList<>();
+        for(int i=0;i<board.length;i++){
+            String str=new String(board[i]);
+            cur.add(str);
         }
-        for (i = row, j = col; i < n && j >= 0; i++, j--) {
-            if (board[i][j] == 'Q')
-                return false;
-        }
-        for (j = col; j >= 0; j--) {
-            if (board[row][j] == 'Q')
-                return false;
-        }
-        return true;
-    }
+        return cur;
+}
 }
